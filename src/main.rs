59d7ccf1381a617d6e7c295eabbd87f34e81541f
Thomas Epperson <thomas.epperson@gmail.com>
2023-08-07 12:24:01 -0500

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use egui_multiwin::multi_window::MultiWindow;
+
+mod windows;
+
+use windows::root::{self};
+
+pub struct AppCommon {}
+
+impl egui_multiwin::multi_window::CommonEventHandler<AppCommon, u32> for AppCommon {
+    fn process_event(
+        &mut self,
+        event: u32,
+    ) -> Vec<egui_multiwin::multi_window::NewWindowRequest<AppCommon>> {
+        let mut windows_to_create = vec![];
+        println!("Received an event {}", event);
+        match event {
+            _ => {}
+        }
+        windows_to_create
+    }
+}
+
+fn main() {
+    let event_loop = egui_multiwin::winit::event_loop::EventLoopBuilder::with_user_event().build();
+    let mut multi_window: MultiWindow<AppCommon, u32> = MultiWindow::new();
+    let root_window = root::RootWindow::new();
+
+    let ac = AppCommon {};
+
+    let _e = multi_window.add(root_window, &event_loop);
+    multi_window.run(event_loop, ac);
+}
---
+++ src/windows/root.rs
@@ -0,0 +1,63 @@
+use egui_multiwin::egui_glow::EguiGlow;
+use egui_multiwin::{
+    multi_window::NewWindowRequest,
+    tracked_window::{RedrawResponse, TrackedWindow},
+};
+
+use crate::AppCommon;
+
+pub struct RootWindow {}
+
+impl RootWindow {
+    pub fn new() -> NewWindowRequest<AppCommon> {
+        NewWindowRequest {
+            window_state: Box::new(RootWindow {}),
+            builder: egui_multiwin::winit::window::WindowBuilder::new()
+                .with_resizable(true)
+                .with_transparent(true)
+                .with_inner_size(egui_multiwin::winit::dpi::LogicalSize {
+                    width: 800.0,
+                    height: 600.0,
+                })
+                .with_title("egui-multiwin root window"),
+            options: egui_multiwin::tracked_window::TrackedWindowOptions {
+                vsync: false,
+                shader: None,
+            },
+        }
+    }
+}
+
+impl TrackedWindow<AppCommon> for RootWindow {
+    fn is_root(&self) -> bool {
+        true
+    }
+
+    fn set_root(&mut self, _root: bool) {}
+
+    fn redraw(
+        &mut self,
+        c: &mut AppCommon,
+        egui: &mut EguiGlow,
+        window: &egui_multiwin::winit::window::Window,
+    ) -> RedrawResponse<AppCommon> {
+        let mut quit = false;
+
+        let mut windows_to_create = vec![];
+
+        egui_multiwin::egui::SidePanel::left("my_side_panel").show(&egui.egui_ctx, |ui| {
+            ui.heading("Hello World!");
+            if ui.button("Quit").clicked() {
+                quit = true;
+            }
+        });
+
+        egui_multiwin::egui::CentralPanel::default().show(&egui.egui_ctx, |ui| {
+            ui.label("I am groot".to_string());
+        });
+        RedrawResponse {
+            quit: quit,
+            new_windows: windows_to_create,
+        }
+    }
+}
