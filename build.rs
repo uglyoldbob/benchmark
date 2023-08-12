311a791d97964c7fc877175a1464f0c130d4760e
Thomas Epperson <thomas.epperson@gmail.com>
2023-08-12 10:57:57 -0500

    Can now find the cpuload shared library.

---
+++ build.rs
@@ -0,0 +1,32 @@
+use std::path::{Path, PathBuf};
+use std::{env, fs, io};
+
+fn find_cargo_target_dir() -> PathBuf {
+    // Infer the top level cargo target dir from the OUT_DIR by searching
+    // upwards until we get to $CARGO_TARGET_DIR/build/ (which is always one
+    // level up from the deepest directory containing our package name)
+    let pkg_name = env::var("CARGO_PKG_NAME").unwrap();
+    let mut out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    loop {
+        {
+            let final_path_segment = out_dir.file_name().unwrap();
+            if final_path_segment.to_string_lossy().contains(&pkg_name) {
+                break;
+            }
+        }
+        if !out_dir.pop() {
+            panic!("Malformed build path: {}", out_dir.to_string_lossy());
+        }
+    }
+    out_dir.pop();
+    out_dir.pop();
+    out_dir
+}
+
+fn main() {
+    let path = find_cargo_target_dir();
+    println!(
+        "cargo:rustc-link-arg=-Wl,-rpath,{}",
+        path.to_str().expect("Link path is not an UTF-8 string")
+    );
+}
